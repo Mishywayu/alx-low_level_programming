2235707c619b0258dd824e5762be214f32a7ed7b
“Mishywayu” <michellewayua2@gmail.com>
2022-09-09 06:22:33 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,10 +6,29 @@
  */
 int main(void)
 {
-	char c;
+	int x, y;
 
-	for (c = 'a'; c <= 'z'; c++)
-	putchar(c);
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (x < y)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
 	putchar('\n');
 	return (0);
 }
