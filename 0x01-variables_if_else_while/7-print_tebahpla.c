52fad548c114721dfb46fe83e3930a9bfb3136e3
“Mishywayu” <michellewayua2@gmail.com>
2022-09-09 05:06:36 -0700

    tabahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int X;
+
+	for (X = 122; X > 96; X--)
+	{
+		putchar(X);
+	}
+	putchar('\n');
+	return (0);
+}
