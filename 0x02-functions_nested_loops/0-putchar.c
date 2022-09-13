e7c7690018f50b2b1e43cd4e8c70d268d5b1d01f
“Mishywayu” <michellewayua2@gmail.com>
2022-09-13 10:19:44 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char sh[8] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+	_putchar(sh[x]);
+	}
+	_putchar('\n');
+	return (0);
+}
