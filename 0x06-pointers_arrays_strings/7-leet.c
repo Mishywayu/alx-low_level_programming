cd92f7d4c2213bfb32e4646dc73323698c8e1366
“Mishywayu” <michellewayua2@gmail.com>
2022-09-22 03:16:20 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 /**
+ * leet - encodes a string into 1337
  *
+ * @s: pointer to string
+ *
+ * Return: pointer to s
+ */
+char *leet(char *s)
+{
+	int stringCount, leetCount;
+	char letters[] = "aAeEoOtTlL";
+	char Num[] = "4433007711";
+
+	stringCount = 0;
+	while (s[stringCount] != '\0')
+	{
+		leetCount = 0;
+
+		while (leetCount < 10)
+		{
+			if (letters[leetCount] == s[stringCount])
+			{
+				s[stringCount] = Num[leetCount];
+			}
+			leetCount++;
+		}
+		stringCount++;
+	}
+	return (s);
+}
