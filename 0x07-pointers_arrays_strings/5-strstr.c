e1057a53c1fdd91331e3adb5207f9e8ebe247544
“Mishywayu” <michellewayua2@gmail.com>
2022-09-26 12:08:43 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#define NULL 0
+/**
+ * _strstr - used to locate and return pointer to first occurence of sunstring
+ *
+ * @haystack: string to search
+ *
+ * @needle: target substring to search
+ *
+ * Return: pointer to index of sring of first occurence.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, y, x;
+	
+	if (needle[0] == '\0')
+		return (haystack);
+
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			x = 1;
+			y = 0;
+
+			while (needle[y] != '\0')
+			{
+				if (haystack[x] == needle[y])
+				{
+					x++;
+					y++;
+				}
+				else
+				{
+					break;
+				}				
+				if (needle[y] == '\0')
+				{
+					return (haystack + i);
+				}
+			}
+			i++
+		}
+		return (NULL);
+	}
+}
