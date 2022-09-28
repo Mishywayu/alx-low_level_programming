114f0b8eecc86bf098d4beed3174224c2abd4919
“Mishywayu” <michellewayua2@gmail.com>
2022-09-28 12:10:50 -0700

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -30,11 +30,11 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-		retun (1);
+		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	else if (*s2 == '*')
 		return (substring_match(s1, (s2 + 1), (s2 + 1)));
 	else
-		return(0);
+		return (0);
 }
