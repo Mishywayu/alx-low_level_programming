fe1dfd9df00c6c37579fb10646f80342625ce334
“Mishywayu” <michellewayua2@gmail.com>
2022-10-27 13:35:16 -0700

    print_listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints content of the string
+ * @h: singly linked list
+ * Return: prints elements of the list (Success)
+ */
+size_t print_listint(const listint_t *h)
+{
+	int a;
+
+	a = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
